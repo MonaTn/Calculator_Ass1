89053dc7e6b0b584cdbdabe057aff764810d8c4f
Mona Torabian <MonA@Monas-MacBook-Pro.local>
2017-02-20 10:20:13 -0500

    Create UI

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -19,7 +19,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var resultLable: UILabel!
+    
+   
+    @IBAction func clearButton(_ sender: UIButton) {
+        resultLable.text = "0"
+    }
+    
+    @IBAction func inverseSign(_ sender: UIButton) {
+        
+        if resultLable.text?.range(of: "-") != nil {
+           resultLable.text?.remove(at: (resultLable.text?.startIndex)!)
+        }
+    }
+    
 }
 
